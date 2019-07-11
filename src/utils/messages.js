1eacdf2a4bf67ebcdc1ce19fd27dcce3449f02d6
Omar <omaralhoori1@gmail.com>
2019-07-11 18:22:03 +0300

    init commit

---
+++ src/utils/messages.js
@@ -0,0 +1,20 @@
+const genrateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const genrateLocationMessage = (username, url) => {
+    return {
+        username,
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+module.exports = {
+    genrateMessage,
+    genrateLocationMessage
+}
